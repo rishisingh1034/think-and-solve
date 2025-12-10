14a99dd7495bd0096b40b618280e739ae63d017a
Rishi Kumar Singh <rishisingh43210@gmail.com>
2025-12-10 23:59:25 +0530

    add case for palin

--- basics-ques/palindrome.js
+++ basics-ques/palindrome.js
@@ -45,3 +45,18 @@ function palindromeString(str) {
 }
 
 console.log(palindromeString("Dad"))
+
+// palindrome
+function palin(x) {
+    let copyX = x;
+    if (x < 0) return false;
+    let rev = 0;
+    while (x > 0) {
+        let rem = x % 10;
+        rev = (10 * rev) + rem;
+        x = Math.floor(x / 10);
+    }
+    return copyX === rev;
+}
+
+console.log(palin(2221))
